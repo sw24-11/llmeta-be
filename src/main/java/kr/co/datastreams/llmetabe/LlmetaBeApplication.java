3e3f5db626a501e112fde14488ff1fc61e1ca99d
rlaekdud <gloria030114@gmail.com>
2024-03-31 21:10:48 +0900

    initial commit

---
+++ src/main/java/kr/co/datastreams/llmetabe/LlmetaBeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.datastreams.llmetabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmetaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmetaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/datastreams/llmetabe/LlmetaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.datastreams.llmetabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmetaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
