cbf64aa9c034d870f2883d9585086619cb3b76ff
Hyewon Seok <hws2008@naver.com>
2024-06-04 10:44:33 +0900

    [Fix: LLMETA-57] Modify annotations of AuthController

--- src/main/java/kr/co/datastreams/llmetabe/api/auth/controller/AuthController.java
+++ src/main/java/kr/co/datastreams/llmetabe/api/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package kr.co.datastreams.llmetabe.api.auth.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.datastreams.llmetabe.api.auth.dto.request.SignupRedundancyCheckRequestDto;
 import kr.co.datastreams.llmetabe.api.auth.dto.request.SignupRequestDto;
 import kr.co.datastreams.llmetabe.api.auth.service.AuthService;
@@ -7,21 +8,24 @@ import kr.co.datastreams.llmetabe.global.utils.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/signup")
+@Tag(name = "인증 컨트롤러")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/signup")
+    @PostMapping()
     public Response<?> signup(@RequestBody SignupRequestDto signupRequestDto) {
         authService.signup(signupRequestDto);
         return Response.ok("성공적으로 회원가입하였습니다.");
     }
 
-    @PostMapping("/signup/redundancyCheck")
+    @PostMapping("/redundancyCheck")
     public Response<?> signupRedundancyCheck(@RequestBody SignupRedundancyCheckRequestDto signupRedundancyCheckRequestDto) {
         authService.signupRedundancyCheck(signupRedundancyCheckRequestDto);
         return Response.ok("중복되지 않은 이메일입니다.");
